6e92a384d81b40bdd4083f709234b37e59ef6d10
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-07-04 07:04:40 +0200

    Use Ananke tests

--- lib/Daedalus/Hermes.pm
+++ lib/Daedalus/Hermes.pm
@@ -9,6 +9,9 @@ use Carp qw(croak);
 use base qw( Class::Factory  );
 use Moose;
 use Moose::Util::TypeConstraints;
+use XML::Parser;
+use XML::SimpleObject;
+
 use namespace::autoclean;
 
 use Data::Dumper;
@@ -19,11 +22,11 @@ Daedalus::Hermes - The great new Daedalus::Hermes!
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
